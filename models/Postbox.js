85f6eaa50dc1e3c0502c2833a74c055b08d50137
skyepaper <111369963+skyepaper@users.noreply.github.com>
2023-05-30 19:04:45 +0300

    Update Postbox.js

--- models/Postbox.js
+++ models/Postbox.js
@@ -4,23 +4,18 @@ const Schema=mongoose.Schema;
 const PostboxSchema=new Schema({
     userId:{
         type:String,
-        required:true
     },
     senderId:{
         type:String,
-        required:true
     },
      type:{
         type:String,
-        required:true
-    },
+    }, 
     status:{
         type:String,
-        required:true
     },
     gold:{
         type:Number,
-        default:0
     }
 })
 
